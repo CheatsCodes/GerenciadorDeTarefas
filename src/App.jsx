be89d9c13e342c2f18f16a7014ebb57599b4841e
Schumacher <schumacheroficial@gmail.com>
2025-04-23 16:16:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Gerenciador de Tarefas</p>
+    </>
+  );
+}
+
+export default App;
