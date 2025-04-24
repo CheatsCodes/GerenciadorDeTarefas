dd47adb628c58e0d8e0d8293bfe70916ebb4ec12
Schumacher <schumacheroficial@gmail.com>
2025-04-24 10:12:33 -0300

    fim do component mock

--- GerenciadorDeTarefas2/src/App.tsx
+++ GerenciadorDeTarefas2/src/App.tsx
@@ -1,12 +1,58 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+interface List {
+  id: number;
+  date: Date;
+  startTime: string;
+  finalTime: string;
+  title: string;
+  description: string;
+  color: string;
+}
 
+const mock: Array<List>[] = [
+  {
+    id: 0,
+    date: new Date("2025-01-20T08:30:00"),
+    startTime: "08:30",
+    finalTime: "09:30",
+    title: "Marketing",
+    description: "3 categories of rocks",
+    color: "orange",
+  },
+  {
+    id: 1,
+    date: new Date("2025-01-19T09:30:00"),
+    startTime: "09:30",
+    finalTime: "11:45",
+    title: "Strategy Planning",
+    description: "Lauch day Bestbuy",
+    color: "purple",
+  },
+  {
+    id: 2,
+    date: new Date("2025-01-20T11:00:00"),
+    startTime: "11:00",
+    finalTime: "12:15",
+    title: "Discovery",
+    description: "Financian project",
+    color: "red",
+  },
+  {
+    id: 3,
+    date: new Date("2025-01-19T14:30:00"),
+    startTime: "14:30",
+    finalTime: "16:20",
+    title: "Developing",
+    description: "Setup Github repository",
+    color: "blue",
+  },
+];
+
+function App() {
   return (
     <>
-      <p>Projeto Gerenciador de Tarefas</p>
+      <p>Gerenciador de Tarefas</p>
     </>
   );
 }
