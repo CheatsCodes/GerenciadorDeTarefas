1dd87fae26fcfd25f18e03e0323cb164b6aa1eab
Schumacher <schumacheroficial@gmail.com>
2025-04-24 15:01:21 -0300

    Função ordenar GerenciadorDeTarefas

--- GerenciadorDeTarefas2/src/shared/components/GerenciadorDeTarefas.tsx
+++ GerenciadorDeTarefas2/src/shared/components/GerenciadorDeTarefas.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 
 export interface List {
     id: number;
@@ -12,8 +13,25 @@ export interface List {
 interface ListProps {
     list: Array<List>
 }
+function sortList(firstDate: List, lastDate: List){
+    if (firstDate.date.getTime() < lastDate.date.getTime())
+        return -1;
+    if (firstDate.date.getTime() > lastDate.date.getTime())
+        return 1;
+    return 0;
+    
+}
+
 function GerenciadorDeTarefas(props: ListProps) {
     const { list } = props;
+
+    useEffect(() => {
+        let sortedList =list.sort(sortList)
+
+        console.log(sortedList);
+    }, [])
+
+
     return(
         <div>{JSON.stringify(list)}</div>
     )
