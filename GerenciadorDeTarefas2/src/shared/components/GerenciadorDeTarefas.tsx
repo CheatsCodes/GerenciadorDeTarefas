76cb559af08b8984aa0319025e9d0714aebea51b
Schumacher <schumacheroficial@gmail.com>
2025-04-25 10:14:36 -0300

    criando-função-GerenciadorDeTarefas-do-componente-funçoões

--- GerenciadorDeTarefas2/src/shared/components/GerenciadorDeTarefas.tsx
+++ GerenciadorDeTarefas2/src/shared/components/GerenciadorDeTarefas.tsx
@@ -1,40 +1,78 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export interface List {
-    id: number;
-    date: Date;
-    startTime: string;
-    finalTime: string;
-    title: string;
-    description: string;
-    color: string
-  }
+  id: number;
+  date: Date;
+  startTime: string;
+  finalTime: string;
+  title: string;
+  description: string;
+  color: string;
+}
 
 interface ListProps {
-    list: Array<List>
+  list: Array<List>;
 }
-function sortList(firstDate: List, lastDate: List){
-    if (firstDate.date.getTime() < lastDate.date.getTime())
-        return -1;
-    if (firstDate.date.getTime() > lastDate.date.getTime())
-        return 1;
-    return 0;
-    
+function sortList(firstDate: List, lastDate: List) {
+  if (firstDate.date.getTime() < lastDate.date.getTime()) return -1;
+  if (firstDate.date.getTime() > lastDate.date.getTime()) return 1;
+  return 0;
+}
+
+function compareDate(lastDate: Date, currentDate: Date): boolean {
+  let yearCondition: boolean =
+    lastDate.getFullYear() == currentDate.getFullYear();
+  let monthCondition: boolean = lastDate.getMonth() == currentDate.getMonth();
+  let dayCondition: boolean = lastDate.getDate() == currentDate.getDate();
+
+  if (yearCondition && monthCondition && dayCondition) {
+    return true;
+  }
+
+  return false;
+}
+
+function createGerenciadorDeTarefas(list: Array<List>): Array<Array<List>> {
+  let newArray: Array<Array<List>> = [];
+  let tempArray: Array<List> = [];
+
+  list.forEach((element, index) => {
+    if (index !== 0) {
+      let checkDate = compareDate(list[index - 1].date, list[index].date);
+
+      if (checkDate) {
+        tempArray.push(element);
+      } else {
+        newArray.push(tempArray);
+        tempArray = [];
+        tempArray.push(element);
+      }
+    } else {
+      tempArray.push(element);
+    }
+
+    if (index == list.length - 1) {
+      newArray.push(tempArray);
+    }
+  });
+
+  return newArray;
 }
 
 function GerenciadorDeTarefas(props: ListProps) {
-    const { list } = props;
+  const { list } = props;
 
-    useEffect(() => {
-        let sortedList =list.sort(sortList)
+  const [GerenciadorDeTarefas, setGerenciadorDeTarefas] = useState(
+    Array<Array<List>>
+  );
 
-        console.log(sortedList);
-    }, [])
+  useEffect(() => {
+    let sortedList = list.sort(sortList);
 
+    setGerenciadorDeTarefas(createGerenciadorDeTarefas(sortedList));
+  }, []);
 
-    return(
-        <div>{JSON.stringify(list)}</div>
-    )
+  return <div>{JSON.stringify(GerenciadorDeTarefas)}</div>;
 }
 
-export default GerenciadorDeTarefas
+export default GerenciadorDeTarefas;
